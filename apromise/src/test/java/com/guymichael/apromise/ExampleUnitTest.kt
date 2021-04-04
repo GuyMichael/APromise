f03e0e2364040abb7f6559d40b14d17d92725ccc
Guy Michael <guy@engieapp.com>
2021-04-04 14:30:28 +0300

    added simple unittest

--- apromise/src/test/java/com/guymichael/apromise/ExampleUnitTest.kt
+++ apromise/src/test/java/com/guymichael/apromise/ExampleUnitTest.kt
@@ -1,17 +1,31 @@
 package com.guymichael.apromise
 
+import com.guymichael.promise.Promise
+import io.reactivex.rxjava3.schedulers.Schedulers
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.concurrent.TimeUnit
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun check_order() {
+        Promise.of(mutableListOf(1))
+            .then { it += 2 }
+            .thenAwait({
+                Thread.sleep(500)
+                Promise.of(it + 3)
+            }, executeOn = Schedulers.computation())
+            .thenMap { l -> l.map { it.toLong() } + 4L }
+            .toObservable()
+            .test()
+            .awaitDone(700, TimeUnit.MILLISECONDS)
+            .assertValue {
+                for (i in 1 until it.size) {
+                    if (it[i] == it[i - 1] + 1) continue
+                    else return@assertValue false
+                }
+                true
+            }
     }
 }
